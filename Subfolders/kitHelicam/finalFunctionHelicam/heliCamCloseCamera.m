eb25d05c901465bff801a85547825b3041e20f1e
Bgandega <boubacar.gandega@ens-paris-saclay.fr>
2025-04-15 11:11:03 +0200

    typo & comment

--- Subfolders/kitHelicam/finalFunctionHelicam/heliCamCloseCamera.m
+++ Subfolders/kitHelicam/finalFunctionHelicam/heliCamCloseCamera.m
@@ -1,7 +1,15 @@
-function heliCamCloseCamera(ObjCamera)
-	heliCamStopAcqMode(ObjCamera);
-	% end of use the camera
-	ObjCamera.c4dev.release(); %end of the interface for the camera
-	ObjCamera.c4if.release();  % cut f the network interface, mandatory to avoid conflict
-	disp("Closed connexion with the Camera");
+function heliCamShutdown(ObjCamera)
+	% function that will correctly disconnected the camera from the computer
+	% If the camera is not correctly disconnected from the computer it prevent
+	% any other software using. It might also stuck the firmware.
+
+	%TODO: check the acquisition mode before doing this, or the programme might crash
+	ObjCamera.c4dev.stopAcquisition();
+	ObjCamera.c4dev.release();
+	ObjCamera.c4if.release();
+
+
+	%TODO: variable management 
+	% do we delete ObjCamera to avoid conflict
+
 end
