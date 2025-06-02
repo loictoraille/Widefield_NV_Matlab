5f5d3fd352c075bafd0ebee52c67cb6d0bf5bde0
Bgandega <boubacar.gandega@ens-paris-saclay.fr>
2025-06-02 16:21:59 +0200

    correction and comment on old functions (might be removed soon)

--- Subfolders/AcqESR/kitHelicam/finalFunctionHelicam/heliCamGetExp.m
+++ Subfolders/AcqESR/kitHelicam/finalFunctionHelicam/heliCamGetExp.m
@@ -1,13 +1,11 @@
 function [exp,expUnit] = heliCamGetExp(ObjCamera)
 %Supposed to return the exposure of the camera but will actually return the "sensitivity" 
-%TODO : maybe recalculate an exposure time from the sensitivity
-% at fixed sensitivity, the exposure time is proportionnal to the period of the demodulation frequency
-% which is not directly responsible to the camera
+%TODO : Remove this function ?
 	
 	sensitivity = ObjCamera.c4dev.readFloat("LockInSensitivity");
 	disp("sensitivity % value is : ");
 	sensitivity % output the value of sensitivity
-	ObjCamera.Realsensitivity = sensitivity; % storing it somewhere	
+	%ObjCamera.Realsensitivity = sensitivity; % storing it somewhere	
 	%TODO: write the correspondence from exposureTime
 	 % to sensitivity
 
