984ec34ca07452fd0f361e277a5e07e8d1fda831
Bgandega <boubacar.gandega@ens-paris-saclay.fr>
2025-06-02 16:21:59 +0200

    correction and comment on old functions (might be removed soon)

--- Subfolders/AcqESR/kitHelicam/finalFunctionHelicam/heliCamSetExp.m
+++ Subfolders/AcqESR/kitHelicam/finalFunctionHelicam/heliCamSetExp.m
@@ -1,8 +1,8 @@
 function heliCamSetExp(ObjCamera,Exposure)
 	% Function to set the exposure
-	% 
+	% TODO : remove this function in the next big change of code
 	sensitivity = Exposure;
 	
 	ObjCamera.c4dev.writeFloat("LockInSensitivity",sensitivity);
-	ObjCamera.sensitivity = sensitivity.
+	ObjCamera.sensitivity = sensitivity
 end
