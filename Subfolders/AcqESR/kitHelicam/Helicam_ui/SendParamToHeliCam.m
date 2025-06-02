a3107fa943e9eea5581ec08ef37aeddabe59169a
Bgandega <boubacar.gandega@ens-paris-saclay.fr>
2025-06-02 16:03:23 +0200

    modification on the way parameters are changed

--- Subfolders/AcqESR/kitHelicam/Helicam_ui/SendParamToHeliCam.m
+++ Subfolders/AcqESR/kitHelicam/Helicam_ui/SendParamToHeliCam.m
@@ -5,7 +5,14 @@ global ObjCamera;
 % and ObjCamera is correctly populated and declared global in heliCamSetParameters if needed there too.
 
 if ~isempty(ObjCamera) % Basic check
-    heliCamSetParameters(ObjCamera);
+
+    %heliCamSetParameters(ObjCamera);
+    if ObjCamera.AcqMode
+    	ObjCamera.AcqMode = false; % in continuous
+    else
+    	heliCamSetParameters(ObjCamera);
+    end
+    
     disp('Parameters sent to HeliCam.');
 else
     disp('Error: Global ObjCamera is empty or not initialized.');
--- Subfolders/AcqESR/kitHelicam/finalFunctionHelicam/heliCamSetParameters.m
+++ Subfolders/AcqESR/kitHelicam/finalFunctionHelicam/heliCamSetParameters.m
@@ -1,65 +1,57 @@
 function heliCamSetParameters(ObjCamera)
-	% Function that set all the parameters of the camera
-	% we change all the parameters at once
-	% Parameters must be set thanks to the c4dev object in the camera
-	% structure, depending on the type of the parameter different parameters must be called
-	
+	% Function that set all parameters of the camera
+	% we change every parameters at oncek	
 
 	%TODO check change in the parameters that need to be updated 
 	% as to not set up everything each time
 	
 	%Parameters that need to be set regularly
 	%sensitivity
-	%phase % TODO: phase exploration is something to look for
+	%phase % TODO: adding phase 
 	
-	%frequence multiplier
-	%NbFrames
-	%Trigger mode
-	%Calibration
-	%AC/DC coupling
 
 
 	if ObjCamera.AcqMode
 		disp("WARNING : AcqMode = true ,the setting of parameters might not work ");
 	end
 	
-    ObjCamera.c4dev.writeString("TriggerSelector", "RecordingStart");
-	  ObjCamera.c4dev.writeString("TriggerMode", "Off");
-	  ObjCamera.c4dev.writeString("TriggerSelector", "FrameStart");
-	  ObjCamera.c4dev.writeString("TriggerMode", "On");
-	  ObjCamera.c4dev.writeString("TriggerSource", "Software");
-	    
-	 % LIA
-	    
-    ObjCamera.c4dev.writeString("DeviceOperationMode", "LockInCam");
-    ObjCamera.c4dev.writeString("Scan3dExtractionMethod", "rawIQ");
-	    
-	  ObjCamera.c4dev.writeFloat("LockInSensitivity", ObjCamera.sensitivity);
-    ObjCamera.c4dev.writeInteger("LockInTargetTimeConstantNPeriods", ObjCamera.NPeriods);
-    ObjCamera.c4dev.writeString("LockInCoupling", ObjCamera.coupling);
-	  ObjCamera.c4dev.writeInteger("LockInExpectedFrequencyDeviation", ObjCamera.expFrequencyDev);
-	  ObjCamera.c4dev.writeFloat("LockInTargetReferenceFrequency", ObjCamera.refFrequency);
-	    
-	  ObjCamera.c4dev.writeString("LockInReferenceSourceType", ObjCamera.refSource);
-	    
-	  % For external reference signal only
-	  ObjCamera.c4dev.writeString("LockInReferenceFrequencyScaler", "Off");
-	  ObjCamera.c4dev.writeString("LockInReferenceSourceSignal",ObjCamera.LockInReferenceSourceSignal);
+	ObjCamera.c4dev.writeString("TriggerSelector", "RecordingStart");
+	ObjCamera.c4dev.writeString("TriggerMode", "Off");
+	ObjCamera.c4dev.writeString("TriggerSelector", "FrameStart");
+	ObjCamera.c4dev.writeString("TriggerMode", "On");
+	ObjCamera.c4dev.writeString("TriggerSource", "Software");
 
-	  %%%% Uncomment this part to test the camera with it's internal generator
-	  %% Illumination
-		% % setup of the generator    
-	  ObjCamera.c4dev.writeFloat("SignalGeneratorOffset", ObjCamera.sgnOffset);
-	  ObjCamera.c4dev.writeFloat("SignalGeneratorAmplitude", ObjCamera.sgnAmplitude);
-	  ObjCamera.c4dev.writeString("LightControllerSelector", "LightController0");
-	  ObjCamera.c4dev.writeString("SignalGeneratorMode", "On");
-	  ObjCamera.c4dev.writeFloat("SignalGeneratorFrequency",ObjCamera.sgnFrequency);
-	  ObjCamera.c4dev.writeString("LightControllerSource", "SignalGenerator");
+	% LIA
 
-	  ObjCamera.c4dev.writeInteger("AcquisitionBurstFrameCount",ObjCamera.NbFrames);
-		ObjCamera.firstSetup = false;
-		disp("heliCamSetParameters dev : default mode to fast");
-		disp("heliCamSetParameters dev : First set of the Camera is default, pause of one seconde");
-		pause(3); %TODO safety measure to let time to the camera to do the setup
-			
+	ObjCamera.c4dev.writeString("DeviceOperationMode", "LockInCam");
+	ObjCamera.c4dev.writeString("Scan3dExtractionMethod", "rawIQ");
+
+	ObjCamera.c4dev.writeFloat("LockInSensitivity", ObjCamera.sensitivity);
+	ObjCamera.c4dev.writeInteger("LockInTargetTimeConstantNPeriods", ObjCamera.NPeriods);
+	ObjCamera.c4dev.writeString("LockInCoupling", ObjCamera.coupling);
+	ObjCamera.c4dev.writeInteger("LockInExpectedFrequencyDeviation", ObjCamera.expFrequencyDev);
+	ObjCamera.c4dev.writeFloat("LockInTargetReferenceFrequency", ObjCamera.refFrequency);
+
+	ObjCamera.c4dev.writeString("LockInReferenceSourceType", ObjCamera.refSource);
+
+	% For external reference signal only
+	ObjCamera.c4dev.writeString("LockInReferenceFrequencyScaler", "Off");
+	ObjCamera.c4dev.writeString("LockInReferenceSourceSignal",ObjCamera.LockInReferenceSourceSignal);
+
+	%%%% Uncomment this part to test the camera with it's internal generator
+	%% Illumination
+	% % setup of the generator    
+	ObjCamera.c4dev.writeFloat("SignalGeneratorOffset", ObjCamera.sgnOffset);
+	ObjCamera.c4dev.writeFloat("SignalGeneratorAmplitude", ObjCamera.sgnAmplitude);
+	ObjCamera.c4dev.writeString("LightControllerSelector", "LightController0");
+	ObjCamera.c4dev.writeString("SignalGeneratorMode", "On");
+	ObjCamera.c4dev.writeFloat("SignalGeneratorFrequency",ObjCamera.sgnFrequency);
+	ObjCamera.c4dev.writeString("LightControllerSource", "SignalGenerator");
+
+	ObjCamera.c4dev.writeInteger("AcquisitionBurstFrameCount",ObjCamera.NbFrames);
+	ObjCamera.firstSetup = false;
+	disp("heliCamSetParameters dev : default mode to fast");
+	disp("heliCamSetParameters dev : First set of the Camera is default, pause of one seconde");
+	pause(3); %TODO reduce this time in continuous mode
+	
 end
