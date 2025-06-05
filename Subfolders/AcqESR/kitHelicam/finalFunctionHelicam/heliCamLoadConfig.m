675da83b015cc242524f35be14492d662ac2a5c7
Bgandega <boubacar.gandega@ens-paris-saclay.fr>
2025-06-05 18:27:34 +0200

    creation of a function to read the config file

---
+++ Subfolders/AcqESR/kitHelicam/finalFunctionHelicam/heliCamLoadConfig.m
@@ -0,0 +1,40 @@
+function heliCamLoadConfig(ObjCamera,path)
+  %function that read the config file of the camera from scratch
+  configFile = fopen(path,'r');
+
+
+
+
+  
+
+ while True
+    configLine = fgetl(configFile); %charge the next line
+
+    if configLine == -1
+      % reach the end of file
+      break; 
+    end
+    formatedLine = split(configLine,["%" "#"])
+
+    if strlength(formatedLine(1)==0 %empty line or commented line
+      continue; 
+    end
+    
+    formatedLine = split(formatedLine(1),["="]);
+
+    if length(formatedLine)<2
+      disp("ignored line config :");
+      disp(configLine);
+      continue;
+    end 
+
+    registerName = formatedLine(1);
+    registerValue = formatedLine(2);
+
+    heliCamSet1Parameter(ObjCamera,registerName,registerValue);
+
+
+   end
+
+
+end
---
+++ Subfolders/AcqESR/kitHelicam/finalFunctionHelicam/heliCamRegisterType.m
@@ -0,0 +1,33 @@
+% This dictionnary contains the type of every register
+% 
+
+
+
+
+
+
+  registerType = dictionnary(
+ "DeviceOperationMode","1",
+ "ChunkModeActive","1",
+ "ChunkEnable","1",
+ "ChunkSelector","1",
+ "LockInTargetReferenceFrequency","1",
+ "LockInReferenceSourceType","1",
+ "TriggerSelector","1",
+ "TriggerMode","1",
+ "TriggerSource","1",
+ "LockInCoupling","2",
+ "LockInSensitivity","",
+ "LockInTargetTimeConstantNPeriods","",
+ "Scan3dExtractionMethod","",
+ "ComponentSelector","",
+ "ComponentEnable","",
+ "AcquisitionBurstFrameCount","",
+ "LightControllerSelector","",
+ "LightControllerSource","",
+ "SignalGeneratorFrequency","",
+ "SignalGeneratorAmplitude","",
+ "SignalGeneratorOffset","",
+ "SignalGeneratorMode",""
+
+ );
